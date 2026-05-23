59533a3ca8054a1983850185c2aa92628c4af6ea
Kuldeep Sharma <kuldeepsharma78607860@gmail.com>
2026-05-23 13:46:03 +0530

    ji

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5002";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://backend:5000";
 
 function App() {
 
